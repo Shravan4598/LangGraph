17c7c29ab603a6717769e9c17636c313ddd200d3
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-17 21:25:39 +0530

    adding streaming to my chatbot

--- 02.ChatBot/app.py
+++ 02.ChatBot/app.py
@@ -24,18 +24,19 @@ if user_input:
     
 
     
-    response=chatbot.invoke(
-        {"messages":[HumanMessage(content=user_input)]},
-        config=config
-    )
+    
 
-    ai_message=response["messages"][-1].content
-
-
+    with st.chat_message("assistant"):
+        ai_message=st.write_stream(
+            message_chunk.content for message_chunk,metadata in chatbot.stream(
+                {
+                    "messages":[HumanMessage(content=user_input)]
+                },
+                config=config,
+                stream_mode="messages"
+            )
+        )
     st.session_state["message_history"].append({
         "role":"assistant",
         "content":ai_message
-    })
-
-    with st.chat_message("assistant"):
-        st.text(ai_message)
+    })   
